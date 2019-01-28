482eef83775b5ceb961df133596ef1063d9db9c7
Winnerawan T <third1337@gmail.com>
2019-01-28 22:30:30 +0700

    done

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>One</title>
+    <title>GO-APLIKASI</title>
     <link rel="stylesheet" href="{{ asset("admin/css/bootstrap.min.css") }}">
     <link rel="stylesheet" href="{{ asset("admin/css/additional.css") }}">
     <link rel="stylesheet" href="{{ asset("admin/css/sidebar.css") }}">    
